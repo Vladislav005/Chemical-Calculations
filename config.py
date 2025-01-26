95b6fc496375791361bb600882e7e209b75ec6f0
ArtyomOl <artem.andreevich.olenev@mail.ru>
2025-01-26 12:55:59 +0300

    V

---
+++ config.py
@@ -0,0 +1,11 @@
+# mysql
+host="localhost"
+user_name="root"
+password="secret"
+db_name="db_chem"
+
+# sqlite
+sqlite_file_name = 'main_database.db'
+
+# connection_types:  global and local
+connection_type = 'local'
