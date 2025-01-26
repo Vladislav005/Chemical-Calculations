0b0fca1060a1ec89a049306a23bcfb77fb0e9135
ArtyomOl <artem.andreevich.olenev@mail.ru>
2025-01-26 12:54:51 +0300

    Z

--- functions.py
+++ functions.py
@@ -20,3 +20,19 @@ class Function:
         return D(D(8.31) * D(temp) * D(x) * (1 - D(x)) * ((1 - D(x)) * D(a1) + D(a2) * D(x)))
 
 margulis = Function("8.31 * temp * x * (1 - x) * ((1 - x) * a1 + a2 * x)")
+
+
+
+
+def derivative(start_initial_params: dict, func: Function):
+    initial_params = start_initial_params.copy()
+    y_0 = func.result(initial_params)
+    dx = 0.00000000001
+    initial_params['x'] = initial_params['x'] + dx
+    dy = func.result(initial_params) - y_0
+    return dy / dx
+
+
+if __name__ == '__main__':
+    func = Function('x**3')
+    print(derivative(start_initial_params={'x': 2}, func=func))
